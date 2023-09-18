b7e55aae938367e649598f6ff4093179321366dd
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-09-18 19:14:32 +0300

    Make the dice holdable

--- components/Die.jsx
+++ components/Die.jsx
@@ -1,6 +1,10 @@
 export default function Die(props){
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "#ffffff"
+    }
+
     return(
-        <div className="dice-box">
+        <div className="dice-box" style={styles} onClick={props.holdDice}>
             <h2>{props.value}</h2>
         </div>
     )
