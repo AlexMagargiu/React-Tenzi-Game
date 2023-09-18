ab37e2609066d7dd11350dd54df6f5bd0c939820
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-09-18 19:44:52 +0300

    Complete tutorial

--- App.jsx
+++ App.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Die from "./components/Die"
 import { nanoid } from 'nanoid'
+import Confetti from "react-confetti"
 
 export default function App(){
     const [dice, setDice] = useState(allNewDice())
+
+    const [tenzies, setTenzies] = useState(false);
+
+    useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+            console.log("You won!")
+        }
+    }, [dice])
 
     function generateNewDie() {
         return {
@@ -21,10 +34,17 @@ export default function App(){
         return newDice
     }
 
-    function rollDice(){
-        setDice(oldDice => oldDice.map(die => {
-            return die.isHeld ? die : generateNewDie() 
-        }))
+    function rollDice() {
+        if(!tenzies) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ? 
+                    die :
+                    generateNewDie()
+            }))
+        } else {
+            setTenzies(false)
+            setDice(allNewDice())
+        }
     }
 
     function holdDice(id){
@@ -37,10 +57,13 @@ export default function App(){
 
     return(
         <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="dice-roll" onClick={rollDice}>Roll</button>
+            <button className="dice-roll" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
         </main>
     )
 }
